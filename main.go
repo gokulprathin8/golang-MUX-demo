d2f2901aab578e2e84ab8d0210e2557895c368ed
Gokul <gokulprathin8@gmail.com>
2021-01-13 10:01:59 +0530

    Added 404 page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:gokulprathin8@gmail.com\">gokulprathin8@gmail.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "We could no find the page you are looking for!")
 	}
 
 }
