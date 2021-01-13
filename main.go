773b5f274f83e68bf192f72c1e2d4f91356601e3
Gokul <gokulprathin8@gmail.com>
2021-01-13 16:08:12 +0530

    Changed Router to Gorilla Mux

--- main.go
+++ main.go
@@ -3,23 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func NotFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:gokulprathin8@gmail.com\">gokulprathin8@gmail.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "We could no find the page you are looking for!")
-	}
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "404 not found!")
+}
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site! </h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:gokulprathin8@gmail.com\">gokulprathin8@gmail.com</a>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":8000", nil)
+	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(NotFound)
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8000", r)
 }
 
